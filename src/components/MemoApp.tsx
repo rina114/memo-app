8407c5b904f5bbd9140a787630e77d88a2e905d6
鈴木里菜 <suzukirina@suzukirkonpyuta.ntkyo1.kn.home.ne.jp>
2024-09-16 22:24:25 +0900

    first commit

--- src/components/MemoApp.tsx
+++ src/components/MemoApp.tsx
@@ -91,8 +91,8 @@ export const MemoApp = () => {
                 </>
             )}
             <ul>
-                {memos.map((memo, index) => (
-                    <div className="flex w-full border border-gray-300 max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800" key={index}>
+                {memos.map((memo) => (
+                    <div key={memo.id} className="flex w-full border border-gray-300 max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
                         <div className="px-4 py-2 -mx-3" >
                             <div className="mx-3" >
                                 <li key={memo.id}>
