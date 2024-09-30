2ee24dba09f505d38793b342e26e863299ee81e3
鈴木里菜 <suzukirina@suzukirkonpyuta.ntkyo1.kn.home.ne.jp>
2024-09-30 20:41:32 +0900

    編集機能・共有機能の追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  // srcディレクトリの全てのファイルにTailwindを適用する 
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
